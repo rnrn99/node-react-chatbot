5159ec11b1e232867daceb23da4a8400abe46ba9
giu0213@naver.com <giu0213@naver.com>
2021-06-23 17:51:08 +0900

    client textQuery 일단 console로 확인만

--- client/src/chatbot/chatbot.js
+++ client/src/chatbot/chatbot.js
@@ -1,6 +1,55 @@
 import React from 'react';
+import axios from 'axios';
 
 function Chatbot() {
+
+    // 서버의 textQuery에 req 보내고 res 처리
+    const textQuery = async (msg) => {
+        
+        // 보낸 메세지 관리
+
+        // msg에대한 form 만들어줌
+        let conversation = {
+            who: 'user',
+            content: {
+                text: {
+                    text: msg
+                }
+            }
+        }
+
+        // 보낼 문장에 대한 form
+        const textQueryVariables = {
+            text: msg
+        }
+
+        // 요청 & 응답 처리
+        try {
+            // textQuery Route에 req 보내기
+            const res = await axios.post('/api/dialogflow/textQuery', textQueryVariables)
+
+            // 요청에 대한 응답이 담긴 부분
+            const content = res.data.fulfillmentMessages[0]
+
+            conversation = {
+                who: 'chatbot',
+                content: content
+            }
+
+            console.log(conversation)
+
+        } catch(error) {
+            conversation = {
+                who: 'chatbot',
+                content: {
+                    text: {
+                        text: "Failed to send message."
+                    }
+                }
+            }
+            console.log("Error : ",conversation)
+        }
+    }
 
     const keyPressHandler = (event) => {
         if(event.key === 'Enter'){
@@ -10,9 +59,9 @@ function Chatbot() {
             }
 
             // 내용 입력 후 엔터 > textQuery에 req 보냄 > input창 비워줌
+            textQuery(event.target.value)
 
             event.target.value = '';
-
         }
     }
 
